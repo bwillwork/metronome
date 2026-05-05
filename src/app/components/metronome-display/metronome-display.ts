fcbc06d7c31d7793cea4447396f088ee04636242
Bryan Williams <bwillwork@proton.me>
2026-05-04 22:50:07 -0400

    Adding code for the mtronome visualization

--- src/app/components/metronome-display/metronome-display.ts
+++ src/app/components/metronome-display/metronome-display.ts
@@ -20,8 +20,9 @@ export class MetronomeDisplay implements AfterViewInit, OnDestroy {
   @ViewChild('canvas')
   public canvasRef!: ElementRef<HTMLCanvasElement>;
 
-  public width = 400;
-  public height = 400;
+  public length;
+  public width;
+  public height;
 
   public beatsPerMinute = input();
 
@@ -34,16 +35,93 @@ export class MetronomeDisplay implements AfterViewInit, OnDestroy {
   );
   public metronome: Signal<number> = toSignal(this.metronome$, { initialValue: 0 });
 
+  private ctx?: CanvasRenderingContext2D;
+
+  constructor() {
+    this.length = 400;
+    this.width = this.length;
+    this.height = this.length;
+  }
+
   ngAfterViewInit() {
     // Access the raw DOM element
     const canvas = this.canvasRef.nativeElement;
-    const ctx = canvas.getContext('2d')!;
-
-    //console.log(rawDiv.innerHTML); // "Raw Element"
-    //rawDiv.style.color = 'red'; // Direct DOM manipulation
+    this.ctx = canvas.getContext('2d')!;
+    this.draw(this.ctx);
   }
 
-  private animationLoop() {}
+  private draw(ctx: CanvasRenderingContext2D) {
+
+    const padding = 20;
+    const center: Point = {
+      x: this.width / 2,
+      y: this.height / 2,
+    };
+    const mainCircle = {
+      c: center,
+      r: this.length / 2 - padding * 2,
+      color: '#ffffff'
+    };
+    this.drawCircle(ctx,mainCircle);
+
+
+    const numCircles = 7;
+    for(let i = 0; i < numCircles; ++i) {
+      const rads = (2 * Math.PI * i) / numCircles - (0.5 * Math.PI);
+      const coords = this.radsToDegrees(rads,center,mainCircle.r);
+      console.log(coords);
+      const circle = {
+        c: coords,
+        r: 30,
+        name: (i + 1).toString(),
+        color: 'skyblue'
+      };
+      this.drawCircle(ctx, circle);
+    }
+
+  }
+
+  private drawCircle(ctx: CanvasRenderingContext2D, circle: CircleData) {
+    ctx.beginPath();
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 5;
+
+    ctx.arc(circle.c.x, circle.c.y, circle.r, 0, (Math.PI / 180) * 360, false);
+    ctx.fillStyle = circle.color;
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+    if(circle.name) {
+      const height = 20;
+      ctx.font = `${height}px serif`;
+      ctx.fillStyle = '#222';
+      const width = ctx.measureText(circle.name).width;
+      const x = circle.c.x - (width/2);
+      const y = circle.c.y + (height/2);
+      ctx.fillText(circle.name, x, y);
+    }
+  }
+
+  private radsToDegrees(rads: number, center: Point, radius: number) {
+    //const r = rads % (Math.PI * 2);
+    return {
+      x: radius * Math.cos(rads) + center.x,
+      y: radius * Math.sin(rads) + center.y,
+    };
+  }
 
   ngOnDestroy(): void {}
 }
+
+export type CircleData = {
+  c: Point,
+  r: number,
+  name?: string,
+  color: string
+};
+
+export type Point = {
+  x: number,
+  y: number
+};
