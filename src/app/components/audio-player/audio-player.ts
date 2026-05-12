5b2902d7f6e0552e84887e88d36f4b00c3fce07d
Bryan Williams <bwillwork@proton.me>
2026-05-12 14:27:46 -0400

    Adding components

---
+++ src/app/components/audio-player/audio-player.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-player',
+  imports: [],
+  templateUrl: './audio-player.html',
+  styleUrl: './audio-player.css',
+})
+export class AudioPlayer {}
---
+++ src/app/components/audio-viz/audio-viz.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-viz',
+  imports: [],
+  templateUrl: './audio-viz.html',
+  styleUrl: './audio-viz.css',
+})
+export class AudioViz {}
